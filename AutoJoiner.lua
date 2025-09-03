af47eccd69b32938f1e1139cf2fb5839ff043192
z99 <roklooter@gmail.com>
2025-09-03 23:34:04 +0800

    Update AutoJoiner.lua

--- AutoJoiner.lua
+++ AutoJoiner.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://gitlab.com/darkiedarkie/dark/-/raw/main/AutoJoiner.lua"))()
+loadstring(game:HttpGet("https://gitlab.com/darkdark/dark/-/raw/main/AutoJoiner.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptsz99/growagarden/refs/heads/main/GuiByKuni"))()
