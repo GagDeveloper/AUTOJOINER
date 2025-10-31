d0e4cbf8f916b82e58570b0bc55c0b2913e8ceab
z99 <roklooter@gmail.com>
2025-10-31 08:13:02 +0800

    Delete AutoJoiner.lua

--- AutoJoiner.lua
+++
@@ -1,2 +0,0 @@
-loadstring(game:HttpGet("https://gitlab.com/darkdark/dark/-/raw/main/AutoJoiner.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptsz99/growagarden/refs/heads/main/GuiByKuni"))()
